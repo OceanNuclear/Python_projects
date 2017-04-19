59f7cdf070401021f663dd166e085421b91b99b8
Ocean <annoyingamygdala@gmail.com>
2017-04-19 19:28:39 +0100

    added comments and fixed a variable mis-naming

--- Image_generation/FNF_lab_grid_production.py
+++ Image_generation/FNF_lab_grid_production.py
@@ -2,16 +2,23 @@
 # coding: utf-8
 from PIL import Image
 
+#This python code is written for the purpose of generating a transparent
+# .png grid to overlay onto any metallurgy micrograph, so that phase-
+# counting can be done.
+#Sharable under the MIT license.
+#-Ocean Wong, April 2017
+
 #img = Image.new('RGB', (2,5))
 #Define a single transparent/green pixle
 trans = (255,255,255,0)
 green = (0, 255, 0, 255)
+#Define the number of division/thickness of each dot/dimension in x/y direction.
 xdiv = 10
 ydiv = 10
-xthick = 6
-ythick = 6
-xdim = 1600
-ydim = 1200
+xthick = 4
+ythick = 4
+xdim = 800
+ydim = 600
 bWidth = int(xdim/xdiv)
 bHeight = int(ydim/ydiv)
 
@@ -23,13 +30,16 @@ midBlock = midBlock*(xdiv-1)
 #midBlock is now 1440 pixels long
 Row = (trans,)*(int(bWidth/2)-xthick)+ (green,)*xthick+ midBlock+ (trans,)*int(bWidth/2)
 #Row is now exactly 1 row of pixels long (1600 pixels)
-row = Row*xthick
+row = Row*ythick
 
+#Make a variable called mid-Co so that it takes up 9 "full columns" in the middle of the picture.
 midCo = (trans,)*(bHeight-ythick)*xdim+ row
 midCo = midCo * (ydiv-1)
 
+#Generate the full picture
 fullPic = (trans,)*(int(bHeight/2)-ythick)*xdim +row + midCo+ (trans,)*int(bHeight/2)*xdim
 
-img3 = Image.new('RGBA', (1600,1200))
+#Output
+img3 = Image.new('RGBA', (xdim,ydim))
 img3.putdata(fullPic)
-img3.save('FNF_array.png', 'PNG')
+img3.save('FNF_array2.png', 'PNG')
