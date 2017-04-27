4ef09c60079b6b06a02ff17d39db092eca1c41e2
Ocean <annoyingamygdala@gmail.com>
2017-04-27 15:08:26 +0100

    Fixed the Ps of Weibull plotting

--- FCMb_statistical/Statistical_analysis.py
+++ FCMb_statistical/Statistical_analysis.py
@@ -3,42 +3,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#USELESS
+#Function to trim away the infinities in the data;
+#def rm_inf(x, y):
+#	x_out = []
+#	y_out = []
+#	for i in range (0,1000):	#runs up to 999 only
+#		if abs(x[i])>100000000 or abs(y[i])>100000000:
+#			x_out.append(0)
+#			y_out.append(0)
+#		else:
+#			x_out.append(x[i])
+#			y_out.append(y[i])
+#	return(x_out, y_out)
 
-#For finding (experimental survival probability)
-def mt(x,y):
-	z = []
-	for i in range (0, 496):
-		No_of_larger = int(0)
-		for j in range (0, 109):
-			if y[j]>i:
-				No_of_larger +=1
-		percentage = float(No_of_larger/109)
-		z.append(percentage)
-	return(z)
-#Well apparently finding Ps is a bit more complicated than just using the experimental probability
-
-
-#function to trim away the infinities in the data; yet to be implemented, but when used can make the code more beautiful.
-def rm_inf(x, y):
-	x_out = []
-	y_out = []
-	for i in range (0,1000):	#runs up to 999 only
-		if abs(x[i])>100000000 or abs(y[i])>100000000:
-			pass
-		else:
-			x_out.append(x[i])
-			y_out.append(y[i])
-	return(x_out, y_out)
-
-
-#Taking in data
-f = open('raw_data', "r")
-f.seek(0)			#because I'm paranoid that the pointer might be in some other place than the start of the file.
-y = [0]*109				#There are 109 entries in the file.
-for i in range (0,109):
-	y[i] = int(f.read(4))
-
-
+#USELSS
 #Overcomplicating part of the code that I decided to give up on.
 #I mean I can use f.tell() to fix this problem of  'can't do nonzero cur-relative seeks' but I mean why should I do that?
 #y = []
@@ -50,40 +29,39 @@ for i in range (0,109):
 #		break
 #numData = len(y)
 
+#Taking in data
+f = open('raw_data', "r")
+f.seek(0)			#because I'm paranoid that the pointer might be in some other place than the start of the file.
+sigma_f = [0]*109		#There are 109 entries in the file.
+for i in range (0,109):
+	sigma_f[i] = int(f.read(4))
 
-y.sort()	#Odering the data set is needed for evalutating Ps.
+
+sigma_f.sort()	#Odering the data set is needed for evalutating Ps.
+#rank of y[i] = i+1
 
 #Find the basic statistical values
-sd = np.std(y)
-mean = np.mean(y)
+sd = np.std(sigma_f)
+mean = np.mean(sigma_f)
 print("Standard Deviation ="+str(sd))	#FYI sd = 118.67416715388205
 print("mean = "+str(mean))
 cv = sd/mean
 print("Coefficient of variation = "+str(cv))
 
 #Plotting part
-#1. Ps(survival probability)
-x = np.linspace(0, 495, 496)
-Ps = mt(x,y)
-plt.plot(x,Ps)
-#plt.show()
-#remove the # above to show the plot
+# 1. We need Ps, -ln(-ln(Ps)), rank, sigma_f, ln(sigma_f)
+Rank = np.linspace(1, 109, 109)
+Ps = 1- (Rank/(109+1))
 
-#2. Weibull's plot
 from numpy import log
-y_axis = -log(-log(Ps))
-x_axis = log(x)
-#truncate away the infinities
-x_2 = [0]*394
-y_2 = [0]*394
-for i in range (101, 495):
-	x_2[i-101] = x_axis[i]
-	y_2[i-101] = y_axis[i]
-plt.plot(x_2, y_2)
-#plt.show()
-#remove the # above to show the plot
+x = log(sigma_f)
+y = -log(-log(Ps))
+
+# 2. Weibull plot
+plt.scatter(x,y)
+plt.show()
 
 #3. Linear regression
 from scipy.stats import linregress
-list_of_data = linregress(x_2,y_2)
+list_of_data = linregress(x,y)
 print(list_of_data)
