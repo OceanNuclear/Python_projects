a770fddec25538df0945526a3844916237a7328a
Ocean <annoyingamygdala@gmail.com>
2017-12-08 23:33:05 +0000

    Changed the demo files location

---
+++ 3D_plot/lines3d_demo.py
@@ -0,0 +1,22 @@
+#!/home/oceanw/anaconda3/bin/python3
+
+#This is the democode downloaded from the online documentation for matplotlib to plot a parametric curve in 3D.
+
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z**2 + 1
+x = r * np.sin(theta)
+y = r * np.cos(theta)
+ax.plot(x, y, z, label='parametric curve')
+ax.legend()
+
+plt.show()
