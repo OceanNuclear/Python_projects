f10b4a0b05d47ea1b0c2c9bc6cff0a2355e5bc5d
Ocean <annoyingamygdala@gmail.com>
2017-12-21 16:08:59 +0000

    Gave SimpleFileCutter.py a more detailed description.

--- Word_editing_graphing/SimpleFileCutter.py
+++ Word_editing_graphing/SimpleFileCutter.py
@@ -1,4 +1,5 @@
 #!/home/oceanw/anaconda3/bin/python3
+#This program extract two particular character from each line of a txt input file and then print them out
 Infile = str(input("Please type in the input filename:\n"))
 Outfile = str(input("Please type in the output filename:\n"))
 numlines = sum(1 for line in open(Infile))
@@ -10,7 +11,8 @@ for n in range (numlines):
 	s[n]=(str(f.readline()))
 	#stored each line of text, will examine them character by character.
 	x = len(s[n])
-	score = str(s[n][x-4])+str(s[n][x-3])
+	score = str(s[n][x-4])+str(s[n][x-3])#Extract the 3rd last and 2nd last character of each line
+	#Note that Enter will be counted as the last character of the line.
 	o.write(score)
 	o.write('\n')
 f.seek(0)
