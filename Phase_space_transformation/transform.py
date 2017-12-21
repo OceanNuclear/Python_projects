a925acf1457581903251dd2167053f6e1d7dbeb2
Ocean <annoyingamygdala@gmail.com>
2017-12-21 15:49:10 +0000

    Added the transform.py file, which is unfinished and intented to use as a plug-in for another .py file.

---
+++ Phase_space_transformation/transform.py
@@ -0,0 +1,10 @@
+#!/home/oceanw/anaconda3/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import scipy.integrate as inte
+
+temp = np.arange(-10, 10)
+#Try parsing the integration to Fortran?
+#Try turning part of the file into a header file, with dependence and such?
