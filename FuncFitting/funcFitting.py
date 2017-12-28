00d0c6e7495addd84cc472190814f8691ce2fbeb
Ocean <annoyingamygdala@gmail.com>
2017-12-28 00:39:53 +0000

    invested several extra hours into it, still got nothing.

--- FuncFitting/funcFitting.py
+++ FuncFitting/funcFitting.py
@@ -11,8 +11,6 @@ y = []
 dy= []
 
 def fittedFunc(x, a, b):
-	print(a*x+b**2)	
-	print(np.sqrt(a*x + b**2))
 	yCalc = np.sqrt(a*x + b**2)/x
 	return yCalc
 
@@ -39,7 +37,8 @@ print("Parsed ", numData, " data points.")
 
 #Next step is to minimize Dev, seen below:
 def Dev (x, y, dy, a, b):
-	return sum((fittedFunc(x, a, b)-y/dy)**2)
+	Deviation= sum(((fittedFunc(x, a, b)-y)/dy)**2)
+	return (Deviation)
 Deviation = np.zeros(zoomAccuracy)
 
 a1 = float (0.0001)
@@ -49,7 +48,9 @@ b2 = float (1)
 b = b1
 #c = c1
 for u in range (loopTime):
+	print("u=",u)
 	for v in range (zoomTime):
+		print("v=",v)
 		if (a1>a2):
 			temp = a2
 			a2 = a1
@@ -57,9 +58,11 @@ for u in range (loopTime):
 			a1 -= (a2-temp)
 			a2 += (a2-temp)
 		else:
-			a1 -= (a2-a1)
-			a2 += (a2-a1)
+			temp = a1
+			a1 -= (a2-temp)
+			a2 += (a2-temp)
 			
+		print("step1a, a1,a2=", a1,a2)
 		aRange = np.linspace(a1, a2, zoomAccuracy)
 		for n in range (zoomAccuracy):
 			Deviation[n] = Dev(x, y, dy, aRange[n], b)
@@ -67,28 +70,33 @@ for u in range (loopTime):
 		while (np.nanargmin(Deviation)==0):
 			for n in range (zoomAccuracy):
 				Deviation[n] = Dev(x, y, dy, aRange[n], b)
-			a1 -= (a2-a1)
-			a2 -= (a2-a1)
+			temp = a1
+			a1 -= (a2-temp)
+			a2 -= (a2-temp)
 			if (a1 == a2): break	
-			print("shuffling value of a to the left to a1, a2 =", a1, a2)
+			try:
+				print("for a, index of min =", np.nanargmin(Deviation), ", just shuffled")
+			except ValueError: pass
 			aRange = np.linspace(a1, a2, zoomAccuracy)#if the Deviation(0) is still min, then the loop will restart
 
-		while (np.nanargmin(Deviation)==9):
+		while (np.nanargmin(Deviation)==(zoomAccuracy-1)):
 			for n in range (zoomAccuracy):
 				Deviation[n] = Dev(x, y, dy, aRange[n], b)
-			a1 += (a2-a1)
-			a2 += (a2-a1)
+			temp = a1
+			a1 += (a2-temp)*0.8
+			a2 += (a2-temp)*0.8
 			if (a1 == a2): break
-			print("shuffling value of a to the right to a1, a2 =", a1, a2)
+			print("For a, index of min =", np.nanargmin(Deviation), ", just shuffled")
 			aRange = np.linspace(a1, a2, zoomAccuracy)
 
 		#Zoom
-		a1 = a1+ (a2-a1)*int(np.argwhere((np.argsort(Deviation)==0)))/10 -(a2-a1)/10
-		a2 = a1+ (a2-a1)*int(np.argwhere((np.argsort(Deviation)==1)))/10
+		temp = a1
+		a1 =temp+ (a2-temp)*int(np.argwhere((np.argsort(Deviation)==0)))/10
+		a2 =temp+ (a2-temp)*int(np.argwhere((np.argsort(Deviation)==1)))/10
 		a = (a1+a2)/2
-		print("v= ",v , ", a = ", a)
+		print("FOr a, index of min =", np.nanargmin(Deviation))
+		print("a1, a2, a =", a1, a2, a)
 
-	for v in range (zoomTime):
 		if (b1>b2):
 			temp = b2
 			b2 = b1
@@ -96,31 +104,38 @@ for u in range (loopTime):
 			b1 -= (b2-temp)
 			b2 += (b2-temp)
 		else:
-			b1 -= (b2-b1)
-			b2 += (b2-b1)
+			temp = b1
+			b1 -= (b2-temp)
+			b2 += (b2-temp)
+		print("step1b, b1,b2=", b1,b2)
 		bRange = np.linspace(b1, b2, zoomAccuracy)	# range of 10 big chunks
 		for n in range (zoomAccuracy):		
 			Deviation[n] = Dev(x, y, dy, a, bRange[n])# list their deviations
 		while (np.nanargmin(Deviation)==0):		#shift according to deviation if index of the smallest one is 0, i.e. left most
 			for n in range (zoomAccuracy):
 				Deviation[n] = Dev(x, y, dy, a, bRange[n])
-			b1 -= (b2-b1)
-			b2 -= (b2-b1)
+			temp = b1
+			b1 -= (b2-temp)
+			b2 -= (b2-temp)
 			if (b1 == b2): break
-			print("shuffling value of b to the left to b1, b2 =", b1, b2)
+			print("for b, index of min =", np.nanargmin(Deviation), " just shuffled")
 			bRange = np.linspace(b1, b2, zoomAccuracy)#re-create range
 		while (np.nanargmin(Deviation)==(zoomAccuracy-1)):
 			for n in range (zoomAccuracy):
 				Deviation[n] = Dev(x, y, dy, a, bRange[n])
-			b1 += (b2-b1)
-			b2 += (b2-b1)
+			temp = b1
+			b1 += (b2-temp)*0.8
+			b2 += (b2-temp)*0.8
 			if (b1 == b2): break
 			bRange = np.linspace(b1, b2, zoomAccuracy)
-			print("shuffling value of b to the right to b1, b2 =", b1, b2)
-		b1 = b1+ (b2-b1)*int(np.argwhere((np.argsort(Deviation)==1)))/10
-		b2 = b1+ (b2-b1)*int(np.argwhere((np.argsort(Deviation)==1)))/10
+			print("For b, index of min =", np.nanargmin(Deviation), " just shuffled")
+		#Zoom
+		temp = b1
+		b1 = temp+ (b2-temp)*int(np.argwhere((np.argsort(Deviation)==0)))/10
+		b2 = temp+ (b2-temp)*int(np.argwhere((np.argsort(Deviation)==1)))/10
+		print("FOr b, index of min =", np.nanargmin(Deviation))
 		b = (b1+b2)/2
-		print("b = ", b)
+		print("b1,b2,b = ",b1,b2, b)
 
 print(a, b)
 a, b
@@ -129,6 +144,10 @@ yCalc = fittedFunc(x,a,b)
 residual = y - yCalc
 
 #Plotting
+print(residual)
+print(Dev(x,y,dy,a,b))
+print(yCalc)
+
 z, (ax1, ax2) = plt.subplots(2, sharex=True)
 ax1.set_title("Custom function fitting")
 ax1.errorbar(x,y, yerr=dy, fmt='.')
