a7a6c08ac2b4814b0bd7b47d6ed5ce587643076a
Ocean <annoyingamygdala@gmail.com>
2018-01-06 02:36:14 +0000

    took away a few files to declutter this folder.

--- Word_editing_graphing/ErrorBarsMult.py
+++ Word_editing_graphing/ErrorBarsMult.py
@@ -5,9 +5,8 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 from scipy.interpolate import spline
 
-inFile = "dummy text"
-while (inFile != str("")):
-	inFile = str(input("Please type in the input filename (without .txt):\n"))
+inFile = str(input("Please type in the input filename (without .txt):\n"))
+while (inFile!= str("")):
 	inFile += str(".txt")
 	numLines = sum(1 for line in open(inFile))
 
@@ -29,13 +28,14 @@ while (inFile != str("")):
 	x = np.array(x)
 	y = np.array(y)
 	dy = np.array(dy)
-
+	xSmooth = np.zeros(0)
 	for dataPoint in range (1, len(x)):
 		xSmooth = np.append( xSmooth, np.linspace( x[dataPoint-1], x[dataPoint], 10))
 	ySmooth = spline(x, y, xSmooth)
 
 	plt.plot(xSmooth, ySmooth)
 	plt.errorbar(x, y, yerr=dy, fmt='.', linewidth=2) #Among all styles of dots, ',' is the smallest, '.' is the second smallest.
+	inFile = str(input("Please type in the input filename (without .txt):\n"))
 xName = str(input("Name of the xaxis?"))
 yName = str(input("Name of the yaxis?"))
 title = str(input("Title?"))
