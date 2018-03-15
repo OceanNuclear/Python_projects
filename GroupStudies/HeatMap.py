Fixed quaternion median finding average method and produced heat maps with crosses to denote actual grain orientations.

GroupStudies/HeatMap.py
@@ -139,6 +139,7 @@ if __name__=="__main__":
 	x, y = np.mgrid[0:tan(pi/8):(xRes*1j), 0:y_max:(yRes*1j)]
 
 	z = griddata(points, rho, (x, y), method='cubic')
+
 	#takes in the (x_data, y_data), z(x_data,y_data), and interpolated data points.
 	#ax.scatter(points[:,0], points[:,1] , color = 'r', marker = 'o')
 
@@ -146,6 +147,7 @@ if __name__=="__main__":
 	print("starting to plot the heat map...")
 	startTime = time.time()
 	graph = ax.pcolor(x,y,z, cmap=cm.jet)#, vmin = 7.5e10, vmax = 1.18e11)
+	ax.scatter(Xco, Yco, color='black')
 	print("Time taken just to plot the main heat map =", time.time()-startTime)
 	#Put white polygons outside of the inverse pole figure area to hide the irrelevant bits.
 	xBound, yBound = InversePoleFigureLine()
@@ -160,6 +162,7 @@ if __name__=="__main__":
 	plt.title("Heat map of dislocation densities at frame "+frameNumstr)
 	plt.colorbar(graph, label = r"$m^{-2}$") #I think this is not part of ax, such that it is plotted outside of the figure.
 
-	#plt.show()
-	plt.savefig("HeatMappable/Frame"+frameNumstr+"DislocationDensity.png")
+	plt.show()
+	#plt.savefig("point1.png")
+	#plt.savefig("HeatMappable/Frame"+frameNumstr+"DislocationDensity.png")
 	#plt.savefig("HeatMappable/Arbitrary Dislocation_OceansCode.png")

GroupStudies/HeatMapLooper.py
@@ -68,7 +68,7 @@ def BG(CompletePoleFig=False):
 for name in ["MaxShearStresses_"]:
 	z = []
 	for frameNumstr in ["120","180","240","300","360"]:
-		z.append( Readrho("MaximumShearStressesOverTime_2/"+name+frameNumstr+"UnaxialNew.txt") )
+		z.append( Readrho("MaximumShearStressesOverTime_3/"+name+frameNumstr+"UnaxialNew.txt") )
 	z_max = np.max(z)
 	z_min = np.min(z)
 
@@ -101,6 +101,7 @@ for name in ["MaxShearStresses_"]:
 		#frameNumstr = "arbitraryData"
 		print("Reading and plotting data for frame", frameNumstr,"for",name)
 		RFinal, AngleFinal = [], []
+		R_ip,	Angle_ip   = [], []
 
 		'''
 		v = [[0., 0., 1.],
@@ -118,8 +119,8 @@ for name in ["MaxShearStresses_"]:
 		RotationMatrices = np.zeros(np.shape(v))
 		'''
 		RealData = True
-		RotationMatrices = ReadR("NewModel/"+frameNumstr+"FrameRotationMatrices.txt")
-		rho = Readrho("MaximumShearStressesOverTime_2/"+name+frameNumstr+"UnaxialNew.txt")[:-1]
+		RotationMatrices = ReadR("OldExtraction/"+frameNumstr+"FrameRotationMatrices.txt")
+		rho = Readrho("MaximumShearStressesOverTime_3/"+name+frameNumstr+"UnaxialNew.txt")
 
 		#Duplicate each point by 24 times:
 		rho = (np.array([rho,]*24).T).ravel()
@@ -127,7 +128,7 @@ for name in ["MaxShearStresses_"]:
 		for n in range (len(RotationMatrices)):
 			if RealData:	v48 = R_v(RotationMatrices[n])
 			else:		v48 = duplicate48Points(v[n][0], v[n][1], v[n][2])
-			pointList = choosePFpoint(v48)
+			pointList  =  choosePFpoint(v48)
 
 			for upVector in pointList:
 				[xl,yl,zl] = upVector
@@ -136,7 +137,16 @@ for name in ["MaxShearStresses_"]:
 				RInt, AngleInt = stereographicProjector(Theta,Phi)
 				RFinal.append(RInt)
 				AngleFinal.append(AngleInt)
-		Xco, Yco = polar2D_xy( AngleFinal, RFinal )
+
+			[x_i,y_i,z_i]=chooseIPpoint(v48)
+
+			Theta, Phi = cartesian_spherical(x_i,y_i,z_i)
+			RInt, AngleInt = stereographicProjector(Theta,Phi)
+			R_ip.append(RInt)
+			Angle_ip.append(AngleInt)
+
+		Xco, Yco = polar2D_xy(AngleFinal, RFinal)
+		Xip, Yip = polar2D_xy( Angle_ip , R_ip  )
 
 		points = np.array([Xco,Yco]).T
 
@@ -153,6 +163,10 @@ for name in ["MaxShearStresses_"]:
 		startTime = time.time()
 		graph = ax.pcolor(x,y,z, cmap=cm.jet, vmin = z_min, vmax = z_max)
 		print("Time taken just to plot the main heat map =", time.time()-startTime)
+
+
+		#Plot the actual orientations over it.
+		ax.plot(Xip, Yip, color='black', linestyle='None', marker = 'x')
 		#Put white polygons outside of the inverse pole figure area to hide the irrelevant bits.
 		xBound, yBound = InversePoleFigureLine()
 
@@ -167,5 +181,5 @@ for name in ["MaxShearStresses_"]:
 		plt.colorbar(graph, label = r"$m^{-2}$") #I think this is not part of ax, such that it is plotted outside of the figure.
 
 		#plt.show()
-		plt.savefig("HeatMappable/Frame"+frameNumstr+name+"WithAbsoluteAndNewExtraction.png")
+		plt.savefig("HeatMappable/MaxShearStress/NewestData/Frame"+frameNumstr+name+"_3WithOldRotationMatrices.png")
 		#plt.savefig("HeatMappable/Arbitrary Dislocation_OceansCode.png")

GroupStudies/WanderOutside.py
@@ -85,7 +85,7 @@ if __name__=="__main__":
 
 	BG()
 
-	RotationMatrices = ReadR("NewModel/1FrameRotationMatrices.txt")
+	RotationMatrices = ReadR("OldExtraction/1FrameRotationMatrices.txt")
 	numGrains = len(RotationMatrices)
 	ID = np.ones(numGrains, dtype=int)*48#The max has range=(0,47), so in theory when ID has been introduced none of them should remain as 48.
 
@@ -106,7 +106,7 @@ if __name__=="__main__":
 			arrowprops=dict(arrowstyle="-",connectionstyle="arc3")
 			)
 		'''
-	preNumFrame=49
+	preNumFrame=44
 	numFrame = 52
 	ax.set_title("Evolution of grains orientations up to frame"+str(numFrame)+"out of 397 frames")
 	x_line = np.zeros([numGrains,numFrame])
@@ -116,7 +116,7 @@ if __name__=="__main__":
 
 	thresholdDistance = 0.2
 	for frame in range (preNumFrame,numFrame):
-		fileName = "NewModel/"+str(frame+1)+"FrameRotationMatrices.txt"
+		fileName = "OldExtraction/"+str(frame+1)+"FrameRotationMatrices.txt"
 		UpdatedMatrices = ReadR(fileName)
 		print("Calculating for frame=", '{:0=3d}'.format(frame+1),"/", numFrame)
 		for grain in range(numGrains):

GroupStudies/generalLibrary.py
@@ -6,6 +6,7 @@ from quat import *
 tau = 2*pi
 from scipy.interpolate.interpolate import interp1d
 #This file does not contain reading functions, as it simply does the job of mathematical processing.
+from numpy import array as ary
 
 
 
@@ -24,13 +25,13 @@ def spherical_cartesian(theta, phi):
 	return [x,y,z]
 
 def cartesian_spherical(x, y, z):
-	x,y,z = np.array([x,y,z], dtype=float) #change the data type to the desired format
+	x,y,z = ary([x,y,z], dtype=float) #change the data type to the desired format
 
 	Theta = arccos(z)
 	Phi = arctan(np.divide(y,x))
 	Phi = np.nan_to_num(Phi)
-	Phi+= np.array( (np.sign(x)-1), dtype=bool)*pi #if x is positive, then phi is on the RHS of the circle; vice versa.
-	return np.array([Theta, Phi])
+	Phi+= ary( (np.sign(x)-1), dtype=bool)*pi #if x is positive, then phi is on the RHS of the circle; vice versa.
+	return ary([Theta, Phi])
 
 
 
@@ -114,7 +115,7 @@ def getTaylorCurveDiv():
 	x = np.linspace(max(xdata), min(xdata),60)
 	f = interp1d(xdata, y)
 	y = f(x)
-	return np.array([x,y])
+	return ary([x,y])
 
 def getTaylorCurve2():
 	xdata = [0.2152941176, 0.1883823529, 0.158394958, 0.1276386555, 0.0930378151, 0.0561302521, 0.0007689076]
@@ -122,7 +123,7 @@ def getTaylorCurve2():
 	x = np.linspace(max(xdata), min(xdata),60)
 	f = interp1d(xdata, y, kind = 'cubic')
 	y = f(x)
-	return np.array([x,y])
+	return ary([x,y])
 
 def getTaylorCurve3():
 	xdata, ydata = [], []
@@ -141,7 +142,7 @@ def getTaylorCurve3():
 		Y.append(  yi )
 		f = interp1d(ydata[n], xdata[n], kind = 'cubic')
 		X.append(f(yi))
-	return np.array([X,Y])
+	return ary([X,Y])
 
 
 #Main functions
@@ -157,24 +158,24 @@ def deNormalize(v):
 	return v/(2*abs(v[2]))
 
 def R_v(R):
-	Z = np.array([0,0,1]) #assume pulling axis is the z axis.
+	Z = ary([0,0,1]) #assume pulling axis is the z axis.
 	[x,y,z] = np.linalg.multi_dot([R,Z]) #see where does the pulling axis lands.
 	#Pick the vector that juts out of the top face:
 	return duplicate48Points(x,y,z)
 
 def duplicate48Points(x0,y0,z0): #Find the equivalent points by permuating the indices and adding negative signs.
-	[x0,y0] = np.array([x0,y0])
+	[x0,y0] = ary([x0,y0])
 	x,y,z = [], [], []
 	
 	for n in range (8):
 		x.append((-1)**(n>>2) *x0); y.append((-1)**(n>>1) *y0); z.append((-1)**(n>>0) *z0)
-		x.append((-1)**(n>>2) *y0); y.append((-1)**(n>>1) *x0); z.append((-1)**(n>>0) *z0)
 		x.append((-1)**(n>>2) *x0); y.append((-1)**(n>>1) *z0); z.append((-1)**(n>>0) *y0)
+		x.append((-1)**(n>>2) *y0); y.append((-1)**(n>>1) *x0); z.append((-1)**(n>>0) *z0)
 		x.append((-1)**(n>>2) *y0); y.append((-1)**(n>>1) *z0); z.append((-1)**(n>>0) *x0)
 		x.append((-1)**(n>>2) *z0); y.append((-1)**(n>>1) *x0); z.append((-1)**(n>>0) *y0)
 		x.append((-1)**(n>>2) *z0); y.append((-1)**(n>>1) *y0); z.append((-1)**(n>>0) *x0)
 
-	return np.array([x,y,z]).T
+	return ary([x,y,z]).T
 
 def chooseIPpoint(arrayOf48pt):
 	for r in arrayOf48pt:
@@ -230,16 +231,16 @@ def ReadR(fileName):
 	Matrices = np.reshape(Matrices, [-1,3])
 	Matrix = []
 	for n in range (len(Matrices)):
-		Matrix.append(	[np.array(Matrices[n][0].split() , dtype=float),
-				np.array( Matrices[n][1].split() , dtype=float),
-				np.array( Matrices[n][2].split() , dtype=float) ] )
+		Matrix.append(	[ary(Matrices[n][0].split() , dtype=float),
+				ary( Matrices[n][1].split() , dtype=float),
+				ary( Matrices[n][2].split() , dtype=float) ] )
 	#np.shape(Matrix) ==(n,3,3)
 	return Matrix
 
 def Readrho(fileName):
 	f = open( str(fileName))
 	rho = f.readlines()
-	rho = np.array(rho, dtype = float)
+	rho = ary(rho, dtype = float)
 	return rho
 
 
@@ -252,4 +253,4 @@ def sphereFillingCurve( m,n , duration, fps=25):
 	theta= sin(r) *pi
 	phi = r*n*tau
 	r = r*tau
-	return np.array([r, theta, phi], dtype=float).T
+	return ary([r, theta, phi], dtype=float).T

GroupStudies/quat.py
@@ -3,6 +3,7 @@ import numpy as np
 import random as rn
 from numpy import sqrt, sin, cos, arccos, pi, arctan
 tau = 2*pi
+from numpy import array as ary
 
 
 
@@ -46,13 +47,13 @@ def spherical_cartesian(theta, phi):
 	return [x,y,z]
 
 def cartesian_spherical(x, y, z):
-	x,y,z = np.array([x,y,z], dtype=float) #change the data type to the desired format
+	x,y,z = ary([x,y,z], dtype=float) #change the data type to the desired format
 
 	Theta = arccos(z)
 	Phi = arctan(np.divide(y,x))
 	Phi = np.nan_to_num(Phi)
-	Phi+= np.array( (np.sign(x)-1), dtype=bool)*pi #if x is positive, then phi is on the RHS of the circle; vice versa.
-	return np.array([Theta, Phi])
+	Phi+= ary( (np.sign(x)-1), dtype=bool)*pi #if x is positive, then phi is on the RHS of the circle; vice versa.
+	return ary([Theta, Phi])
 
 def RootSumSq(array):
 	summation = 0
@@ -93,7 +94,7 @@ def dot (u,v):
 
 def inverse(q):
 	CheckIfQuaternion(q)
-	p = np.array([q[0], -q[1], -q[2], -q[3]])
+	p = ary([q[0], -q[1], -q[2], -q[3]])
 	p = p/(RootSumSq(q)**2)
 	return p
 
@@ -111,7 +112,7 @@ def dotQ(p,q):
 	CheckIfQuaternion(q)
 	return ( p[0]*q[0] + p[1]*q[1] + p[2]*q[2] + p[3]*q[3] )
 
-q0 = np.array([1,0,0,0])
+q0 = ary([1,0,0,0])
 
 def QuatToRotation(q):
 	CheckIfQuaternion(q)
@@ -167,7 +168,7 @@ def writeR(theta, THETA, PHI):
 def matrixMulti(A,B):
 	#optional: check that these two matrices are two dimentional and has the same dimension
 	#if np.shape(A)[1]!=np.shape(B)[0]: raise TypeError
-	#if (np.array(A).ndim!=2) or (np.array(B).ndim!=2): raise TypeError
+	#if (ary(A).ndim!=2) or (ary(B).ndim!=2): raise TypeError
 	vertical=np.shape(A)[0]
 	across = np.shape(B)[1]
 
@@ -180,48 +181,96 @@ def matrixMulti(A,B):
 	return C
 
 def averageQuatLinAlg(qList):
-	qList = np.array(qList)
+	qList = ary(qList)
 	if (np.shape(qList)[1]!=4): raise TypeError#Check that it contains quaternions in the shape of (Mx4).
 
 	Matrix = np.linalg.multi_dot([qList.T,qList])
 	EigenVal, EigenMat= np.linalg.eig(Matrix)
 	average = EigenMat[np.argmax(EigenVal)]
-
-	#CheckUnity(average)
-	#print("Check if ", RootSumSq(average),"is unity.")
 	return average
 
 def averageQuat(qList):
-	qList = np.array(qList)
+	qList = ary(qList)
 	if (np.shape(qList)[1]!=4): raise TypeError#Check that it contains quaternions in the shape of (Mx4).
 
 	Matrix = matrixMulti(qList.T,qList)
 	EigenVal, EigenMat= np.linalg.eig(Matrix)
 	average = EigenMat[np.argmax(EigenVal)]
+	return normalize(average)
 
-	#CheckUnity(average)
-	#print("Check if ", RootSumSq(average),"is unity.")
-	return average
+def medianQuat(qList):
+	qList = ary(qList)
+	if (np.shape(qList)[1]!=4): raise TypeError("It's not of the shape (Nx4)!")
+
+	disorder = np.zeros(len(qList))
+	for n in range(len(qList)):
+		for p in qList:
+			disorder[n] += misorientation2(p,qList[n])/(len(qList)-1)
+	minInd = np.argmin(disorder)
+	return qList[minInd], min(disorder), minInd, max(disorder)
+	#return the quaternion with the minimum disorientation and the mean number of radian difference from the rest of the quaternions
 
 def misorientationR(R1, R2):
 	p = RotToQuat(R1)
 	q = RotToQuat(R2)
-	return misorientationQ(p,q)
+	return misorientation(p,q)
 
 def misorientation(p,q):
-	product = multiply(inverse(p),q)
+	product = multiply(inverse(p), q)
 	return 1-abs(product[0]) #This will be a positive number >0 & <1.
+	#as it returns 1-cos(theta/2)), where theta is the angle required to turn from p to q.
+
+def misorientation2(p,q):#Linear scale of misorientation
+	differenceRotation = multipy(inverse(p), q)	#returns a quaternion
+	differenceRotation = np.clip(differenceRotation, -1,1)	
+	#^clips it back to the range of [-1,1] to correct for any floating point division and multiplication problems.
+	theta = arccos(differenceRotation[0])*2
+	return abs(theta) #return the misorientation in radian.
+	#return 1-cos(theta) #scale it nonlinearly instead.
+
+def generate111s():
+	s3 = sqrt(1/3)
+	x, y, z = [], [], []
+	for n in range (8):
+		x.append((-1)**(n>>2) *s3)
+		y.append((-1)**(n>>1) *s3)
+		z.append((-1)**(n>>0) *s3)
+	return ary([x,y,z]).T	#shape is (8,3)
+
+def misorientation3(p,q, method="radian"): #Misorientation between two quaternions, includes crystal symmetry, super computationally-intensive.
+	v8 = generate111s() # gives 8 vectors, each pointing to a corner.
+	vList1, vList2 = [], []
+	for v in v8:
+		vList1.append( np.linalg.multidot(QuatToR(q),v) ) #Rotate the pose according to p
+		vList2.append( np.linalg.multidot(QuatToR(p),v) ) #and q respectively.
+	misor = 0
+
+	if method=="cosine":
+	#For each vector in the first rotated pose,
+		for u_rotated in vList1:
+			#loop through the vectors of the second pose,
+			misList =  []
+			for v_rotated in vList2:
+				#And find the vector in the second pose that gives a dot product with the first closest to 1
+				misList.append( 1-dot(u_rotated,_rotated) )
+			misor += min(misList)/len(misList)
+
+	if method=="radian":
+		for u_rotated in vList1:
+			misList =  []
+
+			for v_rotated in vList2:
+				misList.append( arccos(np.clip(dot(u_rotated,v_rotated)),-1,1) )
+			misor += min(misList)/len(misList)
+	return misor
 
 def uglyAverage(qList):
-	qList = np.array(qList).T #Gives a 4xM matrix instead this time.
+	qList = ary(qList).T #Gives a 4xM matrix instead this time.
 	average = np.zeros(4)
 
 	for n in range (4):
 		average[n]=np.sum(qList[n])
 	average = average/RootSumSq(average)
-
-	#CheckUnity(average)
-	#print("Check if ", RootSumSq(average),"is unity.")
 	return average
 
 